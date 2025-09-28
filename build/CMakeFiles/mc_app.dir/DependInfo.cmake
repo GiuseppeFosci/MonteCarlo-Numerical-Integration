7fe4fd417ee6d86c7b6346cae2e586d7442aacbd
Giuseppe Fosci <fosci.1855832@studenti.uniroma1.it>
2025-09-28 15:59:05 +0200

    MCN integration

---
+++ build/CMakeFiles/mc_app.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/giuseppe/Esercizi/MonteCarlo-Numerical-Integration/src/PlainMonteCarlo.cpp" "CMakeFiles/mc_app.dir/src/PlainMonteCarlo.cpp.o" "gcc" "CMakeFiles/mc_app.dir/src/PlainMonteCarlo.cpp.o.d"
+  "/home/giuseppe/Esercizi/MonteCarlo-Numerical-Integration/src/StratifiedMonteCarlo.cpp" "CMakeFiles/mc_app.dir/src/StratifiedMonteCarlo.cpp.o" "gcc" "CMakeFiles/mc_app.dir/src/StratifiedMonteCarlo.cpp.o.d"
+  "/home/giuseppe/Esercizi/MonteCarlo-Numerical-Integration/src/main.cpp" "CMakeFiles/mc_app.dir/src/main.cpp.o" "gcc" "CMakeFiles/mc_app.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mc_app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mc_app.dir/src/PlainMonteCarlo.cpp.o"
+  "CMakeFiles/mc_app.dir/src/PlainMonteCarlo.cpp.o.d"
+  "CMakeFiles/mc_app.dir/src/StratifiedMonteCarlo.cpp.o"
+  "CMakeFiles/mc_app.dir/src/StratifiedMonteCarlo.cpp.o.d"
+  "CMakeFiles/mc_app.dir/src/main.cpp.o"
+  "CMakeFiles/mc_app.dir/src/main.cpp.o.d"
+  "mc_app"
+  "mc_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mc_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
